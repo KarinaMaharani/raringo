e48121ab768af004d96acaee74ba433e66e863db
maharani.k4rina21@gmail.com <maharani.k4rina21@gmail.com>
2024-09-09 19:54:39 +0700

    add templates,models,views

--- main/models.py
+++ main/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class MoodEntry(models.Model):
     name = models.CharField(max_length=255)
-    price = models.IntegerField(max_length=255)
+    price = models.IntegerField()
     description = models.TextField()
     tags = models.CharField(max_length=255)
-    ratings = models.DecimalField(max_length=2)
+    ratings = models.DecimalField(max_digits=2, decimal_places=1)
 
     @property
     def recommend_to_user(self):
--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'name' : 'Eyeshadow Parastyles X Gone',
+        'price': 75000,
+        'description': 'Trio Best Selling Shades in our combined customer base',
+        'tags': 'BEAUTY, LIFESTYLE',
+        'ratings': 4.3
+
+    }
+
+    return render(request, "main.html", context)
